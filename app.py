c063d5617346548e1972eb63ea20769ad6c56d62
TurraCx <hello@lqd.pw>
2022-01-09 08:55:37 +0100

    changed download methods and some css

--- app.py
+++ app.py
@@ -8,17 +8,14 @@ d = DanishBytes.DanishBytes()
 def hello(name=None):
     return render_template('hello.html')
 
+@app.route('/mirrors', methods=['GET'])
+def mirrors():
+    return jsonify(d.check_mirrors())
+
 @app.route('/search', methods=['POST'])
 def search():
     d.set_api(request.json['api_key'])
-    return jsonify(d.find_movie(request.json['search'])['torrents'])
+    return jsonify(d.find_movie(request.json['search']))
 
-@app.route('/download', methods=['POST'])
-def download():
-    return jsonify(d.get_torrent(request.json['item']['id'])['attributes'])
-
-@app.route('/magnet', methods=['POST'])
-def magnet():
-    tor = d.get_torrent(request.json['item']['id'])
-    magnet = f"magnet:?dn={tor['attributes']['name']}&xt=urn:btih:{request.json['item']['info_hash']}&as={tor['attributes']['download_link']}&xl={tor['attributes']['size']}&tr=https://danishbytes.club/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes.org/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes2.org/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes.art/announce/e064ba0c35d252338572fd7720448cc5"
-    return magnet
+if __name__ == "__main__":
+    app.run(port=3000)
--- static/app.js
+++ static/app.js
@@ -3,26 +3,36 @@ const vm = new Vue({
     delimiters: ['[[', ']]'],
     data: {
         api_key: "",
+        rss_key: "",
         search: "",
-        torrents: []
+        torrents: [],
+        mirrors: [
+            {
+                name: 'DanishBytes.club',
+                link: 'https://danishbytes.club'
+            }
+        ],
+        news: "Ingenting"
+    },
+    mounted() {
+        axios.get('/mirrors').then(resp => {
+            this.mirrors = resp.data;
+        });
     },
     methods: {
         searchTorrents() {
             if (this.api_key.length > 0) {
                 axios.post('/search', { search: this.search, api_key: this.api_key }, { headers: { 'Content-Type': 'application/json' } }).then(resp => {
-                    this.torrents = resp.data;
+                    this.torrents = resp.data.torrents;
+                    this.rss_key = resp.data.rss_key;
                 });
             }
         },
         downloadTorrent(item) {
-            axios.post('/download', { item: item }, { headers: { 'Content-Type': 'application/json' } }).then(resp => {
-                window.open(resp.data.download_link)
-            });
+            window.open(this.mirrors[0].link+"/torrent/download/"+item.id+"."+this.rss_key);
         },
         generateMagnet(item) {
-            axios.post('/magnet', { item: item }, { headers: { 'Content-Type': 'application/json' } }).then(resp => {
-                window.open(resp.data)
-            });
+            window.open("magnet:?dn="+item.name+"&xt=urn:btih:"+item.info_hash+"&as="+this.mirrors[0].link+"/torrent/download/"+item.id+"."+this.rss_key+"&xl="+item.size+"&tr=https://danishbytes.club/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes.org/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes2.org/announce/e064ba0c35d252338572fd7720448cc5&tr=https://danishbytes.art/announce/e064ba0c35d252338572fd7720448cc5");
         }
     }
 })
